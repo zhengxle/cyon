763fe82a5ab5516e5ab49e928bba68dfddb8d4ab
Joris Vink <joris@coders.se>
2014-02-10 00:11:48 +0100

    Add initial master->slave sync script

---
+++ scripts/cyon-sync.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+# Master to slave sync script. Figures out the state of the given
+# slave and scp's over the correct log for it.
+#
+# Copyright (c) 2014 Joris Vink <joris@coders.se>
+# Part of Cyon (https://github.com/jorisvink/cyon)
+#
+
+MASTER="127.0.0.1:2222"
+SLAVE="127.0.0.1:2223"
+LOCAL="/home/joris/db"
+REMOTE="/home/joris/db-slave"
+DBNAME="sync-test"
+
+STATE=`./cyon-cmd -s ${SLAVE} stats | grep "Current" | awk '{ print $3 }'`
+
+if [ $? -ne 0 ]; then
+	logger -p daemon.info -t cyon-sync "Failed to grab latest state";
+	exit 1;
+fi
+
+if [ ! -f ${LOCAL}/${DBNAME}.${STATE} ]; then
+	logger -p daemon.crit -t cyon-sync "No such state on master: ${STATE}";
+	exit 1;
+fi
+
+# XXX - We have to give the master time to write its store.
+# XXX - We need a better way of checking when it's done
+./cyon-cmd -s ${MASTER} write
+if [ $? -ne 0 ]; then
+	logger -p daemon.info -t cyon-sync "Failed to issue write to master";
+	exit 1;
+fi
+
+sleep 10;
+
+logger -p daemon.info -t cyon-sync "Preparing to sync state: ${STATE}";
+cp -p ${LOCAL}/${DBNAME}.${STATE} ${REMOTE}/
+if [ $? -ne 0 ]; then
+	logger -p daemon.info -t cyon-sync "Copy of state ${STATE} has failed";
+	exit 1;
+fi
+
+logger -p daemon.info -t cyon-sync "Applying state ${STATE} to slave";
+./cyon-cmd -s ${SLAVE} replay ${STATE}
+if [ $? -ne 0 ]; then
+	logger -p daemon.info -t cyon-sync "Slave has failed to apply ${STATE}";
+	exit 1;
+fi
+
+./cyon-cmd -s ${SLAVE} write
+if [ $? -ne 0 ]; then
+	logger -p daemon.info -t cyon-sync "Failed to issue write to slave";
+fi
+
+logger -p daemon.info -t cyon-sync "Slave synchronized";
